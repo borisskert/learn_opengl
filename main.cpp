be605e2de702ad892cb114ab2ae3cd8eb280b251
Boris Skert <boris.skert@gmail.com>
2020-12-20 22:43:42 +0100

    Add some comments and little improvements

--- main.cpp
+++ main.cpp
@@ -3,22 +3,40 @@
 #include <GLFW/glfw3.h>
 #include "src/RuntimeException.h"
 
+// settings
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
+/**
+ * glfw: whenever the window size changed (by OS or user resize) this callback function executes
+ * @param window pointer to the window object
+ * @param width the screen width in pixels
+ * @param height the screen height in pixels
+ */
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
+/**
+ * glfw: initialize and configure
+ */
 void initializeGlfwWindow() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // for MacOS
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
 }
 
+/**
+ * glfw window creation
+ * @return pointer to the created window object
+ */
 GLFWwindow *createGlfwWindow() {
-    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == nullptr) {
         glfwTerminate();
         throw RuntimeException("Failed to create GLFW window");
@@ -28,6 +46,9 @@ GLFWwindow *createGlfwWindow() {
     return window;
 }
 
+/**
+ * glad: load all OpenGL function pointers
+ */
 void initializeGlad() {
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
         throw RuntimeException("Failed to initialize GLAD");
@@ -35,12 +56,27 @@ void initializeGlad() {
 }
 
 void initializeViewport(GLFWwindow *window) {
-    glViewport(0, 0, 800, 600);
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 }
 
+/**
+ * process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+ * @param window pointer to the window object
+ */
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+/**
+ * render loop
+ * @param window pointer to the window object
+ */
 void runEngine(GLFWwindow *window) {
     while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
