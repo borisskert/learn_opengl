496539cc645cf5f362687759295736fd3134ae54
Boris Skert <boris.skert@gmail.com>
2020-12-20 22:31:48 +0100

    Clean up code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "src/RuntimeException.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-int main() {
+void initializeGlfwWindow() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -15,32 +15,55 @@ int main() {
 
     // for MacOS
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+}
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
+GLFWwindow *createGlfwWindow() {
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == nullptr) {
         glfwTerminate();
-        return -1;
+        throw RuntimeException("Failed to create GLFW window");
     }
     glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+    return window;
+}
+
+void initializeGlad() {
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        throw RuntimeException("Failed to initialize GLAD");
     }
+}
 
+void initializeViewport(GLFWwindow *window) {
     glViewport(0, 0, 800, 600);
-
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+}
 
-    while(!glfwWindowShouldClose(window))
-    {
+void runEngine(GLFWwindow *window) {
+    while (!glfwWindowShouldClose(window)) {
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
+}
+
+int main() {
+    try {
+        initializeGlfwWindow();
+
+        GLFWwindow *window = createGlfwWindow();
+
+        initializeGlad();
+
+        initializeViewport(window);
+
+        runEngine(window);
+    }
+    catch (const RuntimeException &exception) {
+        std::cout << "Error occurred: " << exception.getMessage() << std::endl;
+        return -1;
+    }
+
     return 0;
 }
---
+++ src/RuntimeException.h
@@ -0,0 +1,23 @@
+#ifndef MYFIRSTOPENGL_RUNTIMEEXCEPTION_H
+#define MYFIRSTOPENGL_RUNTIMEEXCEPTION_H
+
+#include <string>
+#include <utility>
+
+class RuntimeException : std::exception {
+private:
+    std::string message;
+
+public:
+    RuntimeException() : std::exception() {}
+
+    explicit RuntimeException(std::string message)
+            : std::exception(),
+              message(std::move(message)) {}
+
+    std::string getMessage() const {
+        return this->message;
+    }
+};
+
+#endif //MYFIRSTOPENGL_RUNTIMEEXCEPTION_H
